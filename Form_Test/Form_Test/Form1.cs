466f10bc2ac81138cef644e57c4de1e2ab51bde7
YABE-MIZUKI <zeal25360038@fsgcl.jp>
2025-10-24 15:35:57 +0900

    練習問題

--- Form_Test/Form_Test/Form1.cs
+++ Form_Test/Form_Test/Form1.cs
@@ -15,6 +15,32 @@ namespace Form_Test
         public Form1()
         {
             InitializeComponent();
+
+           
+
+            for (int i = 0; i < 6; i++)
+            {
+                for (int d = 0; d < 4; d++)
+                {
+                    Testbutton testbutton = new Testbutton();
+
+                    //ボタンの位置
+                    testbutton.Location = new Point(50 * i, 50*d);
+                    //ボタンのサイズ変更
+                    testbutton.Size = new Size(50, 50);
+                    //ボタンのテキスト変更
+                    testbutton.Text = "府中";
+
+                    //コントロールにボタンを追加
+                    Controls.Add(testbutton);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("マダガスカル！\nでも普通じゃない？\n府中！！");
+
         }
     }
 }
